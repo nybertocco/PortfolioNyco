5510bd078e4ebc85afc3842a8123951c10dbac6c
Nycolas <nybertocco@gmail.com>
2025-03-24 15:40:40 -0300

    Portfolio finalizado

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import { LiaCopyrightSolid } from "react-icons/lia";
+import "./index.css";
+
+export default function Footer() {
+  return (
+    <footer className="Footer-component">
+      <span>
+        <LiaCopyrightSolid />
+      </span>
+
+      <p className="p-footer">2025 - Nycolas Bertocco</p>
+    </footer>
+  );
+}
